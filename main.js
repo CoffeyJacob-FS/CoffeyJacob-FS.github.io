2414bc3260a55161414860785af4b3cfbbb67950
CoffeyJacob-FS <jacoffey@Student.Fullsail.edu>
2021-06-27 12:00:17 -0400

    Final

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-/*
-* basic idea of game is that each car listed on the site gets a random number assigned to it,
-* once all numbers are assigned they are compared against each other in 1v1 one "races" until there is one winner.
-* the user can guess on each race who will win.
-*
-* {add all cars to a dictionary with the following template:
-* key/value
-*car name / random number
-* }
-*
-* {races:
-*   start by racing the first and last entries, the winners get added to a list.
-*       two counters, one counting up and one counting down once the are equal the loop stops.
-*           next loop will cycle through the winners list and remove the losers.
-*               before each race the user will be prompted to " bet on a winner"
-*
-*
-* }
-*       winner is chosen by the last one in the winners list.
-*
-* */
+
+window.onload = function () {
+    var carList = ["BRZ", "Grom", "Miata", "300Z", "r34", "RX7", "s2000", "Viper", "Golf", "WRX"]
+    var Cars = {};
+    carList.forEach(function (car) {
+        var multiplyer=Math.random();
+        Cars[car] = Math.random()*multiplyer;
+
+    })
+    var raceStart = document.getElementById("RaceB").onclick = Race;
+
+    function Race() {
+        var Car1 = document.getElementById('Racer1').value;
+        var Car2 = document.getElementById('Racer2').value;
+        var output = document.getElementById("raceWinner");
+        var Winner;
+        console.log(Cars[Car1]);
+        console.log(Cars[Car2]);
+        if (Cars[Car1] > Cars[Car2]) {
+            Winner = Car1;
+        }
+        if (Cars[Car1] < Cars[Car2]) {
+            Winner = Car2;
+        }
+        if(Cars[Car1]===Cars[Car2]){
+            Winner="TIE!";
+        }
+
+
+        output.innerHTML = "The winner is:" + Winner;
+
+    }
+}
